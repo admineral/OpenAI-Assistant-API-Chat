084e1ff407437e133abef66cbca662ca3e0518ee
Elias <50579369+admineral@users.noreply.github.com>
2023-11-08 07:37:31 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ChatHN – Chat with Hacker News using natural language",
+  title: "Agentus Smith",
   description:
-    "Chat with Hacker News using natural language. Built with OpenAI Functions and Vercel AI SDK.",
+    "Built with OpenAI Assistant API",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
