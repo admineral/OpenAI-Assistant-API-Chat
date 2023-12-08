11dec33c21ddf8c3b5d9da5c2cea3a9ca5344d6f
admineral <50579369+admineral@users.noreply.github.com>
2023-12-08 18:16:25 +0100

    testing Speedinsights form vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,14 @@ import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 import Toaster from "./toaster";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"; // Import SpeedInsights
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Agent42",
-  description:
-    "OpenAI Assistant",
-    metadataBase: 'https://mydomain.com'
+  description: "OpenAI Assistant",
+  metadataBase: 'https://mydomain.com'
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -19,6 +19,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className={inter.className}>
         {children}
         <Toaster />
+        <SpeedInsights /> {/* Add SpeedInsights */}
       </body>
       <Analytics />
     </html>
